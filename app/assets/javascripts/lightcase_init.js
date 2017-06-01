b2699042269e3271bbeff73e45b5de1246d84a2d
billrozy <you@example.com>
2017-06-01 22:29:31 +0300

    js minify

--- app/assets/javascripts/lightcase_init.js
+++ app/assets/javascripts/lightcase_init.js
@@ -1,6 +1,3 @@
-/**
- * Created by FD on 23.03.2017.
- */
 var ready = function() {
     $('a[data-rel="lightcase:myCollection:slideshow"]').lightcase({showSequenceInfo: false, transition: 'scrollHorizontal'});
     $('a[data-rel="lightcase"]').lightcase();
